53b52e46155d56eff88ef2218ed8fe3d044df991
KETTLE <giwon1515@naver.com>
2023-07-27 09:17:16 +0900

    Setting Init

---
+++ resources/js/script.js
@@ -0,0 +1,23 @@
+const dd = console.log;
+
+const App = {
+
+  init(){
+    App.hook();
+  },
+
+  hook(){
+    $(document)
+  }
+
+}
+
+const Map = {
+
+  init(){
+    
+  }
+  
+}
+
+$(() => App.init();)
